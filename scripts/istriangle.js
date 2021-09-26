84f8990b4cf576f212209e75ba342581885449da
ArjunGTX <arjunatpavilion@gmail.com>
2021-09-26 20:50:26 +0530

    fix: fixed validation on istriangle
    fixed condition when angle is less than 0 and greater than 180

--- scripts/istriangle.js
+++ scripts/istriangle.js
@@ -23,10 +23,13 @@ function validate() {
         if(!angleInputs[0].value || !angleInputs[1].value || !angleInputs[2].value ) {
             flag = 0;
             showOutput('Enter all the angles.');
-        } else if(angleInputs[0].value < 0 || angleInputs[1].value < 0 || angleInputs[2].value < 0) {
+        } else if(angleInputs[0].value <= 0 || angleInputs[1].value <= 0 || angleInputs[2].value <= 0) {
             flag = 0;
-            showOutput('Angles cannot be negative.');
-        } else {
+            showOutput('Angles must be greater than 0.');
+        } else if(angleInputs[0].value > 180 || angleInputs[1].value > 180 || angleInputs[2].value >180) {
+            showOutput('Angles must be less than 180°')
+        }
+        else {
             flag = 1;
         }
     if(flag === 0) {
